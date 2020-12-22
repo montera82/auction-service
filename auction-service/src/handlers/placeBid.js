addbb59b5ff42f31a2d277f3edc5569dd9de9baf
Edwin o <okonkwo.ikechukwu01@gmail.com>
2020-12-22 02:16:56 +0100

    Associating the user that placedBid.

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -11,22 +11,37 @@ async function placeBid(event, context) {
    
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
+
+  // Cannot Bid on your own auction
+  if ( email === auction.seller ){
+    throw new createError.Forbidden(`You cannot bid on your own auction!`);
+  }
+
+  // Cannot double bid business rule
+  if ( email === auction.highestBid.bidder ){
+    throw new createError.Forbidden(`You are already the highest bidder!`);
+  }
+
+  // Amount can not be less business rule
   if ( amount < auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`);
   }
 
+  // Status must be open validation
   if ( auction.status != 'OPEN' ) {
     throw new createError.Forbidden(`You cannot bid on a closed auction`);
   }
 
   const params = {
     TableName : process.env.AUCTIONS_TABLE_NAME,
-    Key: {id},
-    UpdateExpression : 'set highestBid.amount = :amount',
+    Key: { id },
+    UpdateExpression : 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
-      ':amount': amount
+      ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues : 'ALL_NEW',
   }
