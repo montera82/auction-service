a2bb957a4719e668dbbffa00cb5f9688a47c241a
Edwin o <okonkwo.ikechukwu01@gmail.com>
2020-12-24 02:19:26 +0100

    Integrating SQS to handle mail queue.

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,41 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: process.env.REGION});
 
 async function sendMail(event, context) {
-  return event;
+
+  const record = event.Records[0];
+  console.log ('record processing->', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
+  const params = {
+    Source : process.env.SourceEmail,
+    Destination : {
+      ToAddresses : [
+        recipient
+      ]
+    },
+      Message : {
+        Body: {
+          Text: {
+            Data: body,
+          },
+        },
+        Subject: {
+          Data: subject
+        }
+      },
+    };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
